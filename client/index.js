1722f6b33b09ef5d31c1dbe1fc888c4c5e986c1d
thienmdoan <thienmdoan@gmail.com>
2017-02-10 18:16:14 -0800

    Refactor.

--- client/index.js
+++ client/index.js
@@ -12,80 +12,76 @@ entity.addEventListener('mouseenter', function() {
 var num = Math.floor(Math.random() * 20);
 
 
-const renderThreeD = () => {
-  var sceneEl = document.getElementById('scene');
-  var entityEl = document.createElement('a-plane');
-  entityEl.setAttribute('color', 'tan');
-  entityEl.setAttribute('height', '100');
-  entityEl.setAttribute('width', '100');
-  entityEl.setAttribute('rotation', '-90 0 0');
+function renderThreeD() {
+  var $sceneEl = document.getElementById('scene');
+  var $entityEl = document.createElement('a-plane');
+  $entityEl.setAttribute('color', 'tan');
+  $entityEl.setAttribute('height', '100');
+  $entityEl.setAttribute('width', '100');
+  $entityEl.setAttribute('rotation', '-90 0 0');
 
-  var entityVid = document.createElement('a-video');
-  entityVid.setAttribute('src', '#video');
-  entityVid.setAttribute('width', '16');
-  entityVid.setAttribute('height', '9');
-  entityVid.setAttribute('position', '0 5 -10');
-  entityVid.setAttribute('on', 'click');
+  var $entityVid = document.createElement('a-video');
+  $entityVid.setAttribute('src', '#video');
+  $entityVid.setAttribute('width', '16');
+  $entityVid.setAttribute('height', '9');
+  $entityVid.setAttribute('position', '0 5 -10');
+  $entityVid.setAttribute('on', 'click');
 
-  var entityControl = document.createElement('a-entity');
-  entityControl.setAttribute('position', '0 0 0');
-  entityControl.setAttribute('camera');
-  entityControl.setAttribute('look-controls');
-  entityControl.setAttribute('wasd-controls');
+  var $entityControl = document.createElement('a-entity');
+  $entityControl.setAttribute('position', '0 0 0');
+  $entityControl.setAttribute('camera');
+  $entityControl.setAttribute('look-controls');
+  $entityControl.setAttribute('wasd-controls');
 
-  var plane1 = document.createElement('a-image');
-  plane1.setAttribute('src', 'patagonia.jpg');
-  plane1.setAttribute('position', '0 2.5 -6');
-  plane1.setAttribute('height','4');
-  plane1.setAttribute('width', '5');
-  plane1.setAttribute('id', 'patagonia');
+  var $plane1 = document.createElement('a-image');
+  $plane1.setAttribute('src', 'patagonia.jpg');
+  $plane1.setAttribute('position', '0 2.5 -6');
+  $plane1.setAttribute('height','4');
+  $plane1.setAttribute('width', '5');
+  $plane1.setAttribute('id', 'patagonia');
+  $plane1.setAttribute('class', 'location');
 
-  var plane2 = document.createElement('a-image');
-  plane2.setAttribute('src', 'yosemite.jpg');
-  plane2.setAttribute('position', '-5.5 2.5 -4');
-  plane2.setAttribute('height','4');
-  plane2.setAttribute('width', '5');
-  plane2.setAttribute('rotation', '0 40 0');
-  plane2.setAttribute('id', 'yosemite');
+  var $plane2 = document.createElement('a-image');
+  $plane2.setAttribute('src', 'yosemite.jpg');
+  $plane2.setAttribute('position', '-5.5 2.5 -4');
+  $plane2.setAttribute('height','4');
+  $plane2.setAttribute('width', '5');
+  $plane2.setAttribute('rotation', '0 40 0');
+  $plane2.setAttribute('id', 'yosemite');
+  $plane2.setAttribute('class', 'location');
 
-  var plane3 = document.createElement('a-image');
-  plane3.setAttribute('src', 'joshuatree.jpg');
-  plane3.setAttribute('position', '5.5 2.5 -4');
-  plane3.setAttribute('height','4');
-  plane3.setAttribute('width', '5');
-  plane3.setAttribute('rotation', '0 -40 0');
-  plane3.setAttribute('id', 'joshuatree');
+  var $plane3 = document.createElement('a-image');
+  $plane3.setAttribute('src', 'joshuatree.jpg');
+  $plane3.setAttribute('position', '5.5 2.5 -4');
+  $plane3.setAttribute('height','4');
+  $plane3.setAttribute('width', '5');
+  $plane3.setAttribute('rotation', '0 -40 0');
+  $plane3.setAttribute('id', 'joshuatree');
+  $plane3.setAttribute('class', 'location');
 
-  var sky = document.createElement('a-sky');
-  sky.setAttribute('src', '#skyTexture');
-/*
-//  for (let i = 0; i < 5; i++){
+  var $sky = document.createElement('a-sky');
+  $sky.setAttribute('src', '#skyTexture');
 
-    var tree = document.createElement('a-collada-model');
-    tree.setAttribute('src','#tree');
-    tree.setAttribute('position', '10, 0, -5');
-    sceneEl.appendChild(tree);
-    var tree6 = document.createElement('a-collada-model');
-    tree.setAttribute('src','#tree');
-    tree.setAttribute('position', '-10, 0, -5');
-    sceneEl.appendChild(tree6);
-//  }
-*/
-  sceneEl.appendChild(entityEl);
-  sceneEl.appendChild(sky);
-  //sceneEl.appendChild(entityVid);
-  sceneEl.appendChild(entityControl);
+  var $animation = document.createElement('a-animation');
+  $animation.setAttribute('attribute', 'rotation');
+  $animation.setAttribute('begin', 'click');
+  $animation.setAttribute('to','360 0 0');
 
-  entityControl.appendChild(plane1);
-  entityControl.appendChild(plane2);
-  entityControl.appendChild(plane3);
-  return sceneEl;
+  $sceneEl.appendChild($entityEl);
+  $sceneEl.appendChild($sky);
+  //$sceneEl.appendChild($entityVid);
+  $sceneEl.appendChild($entityControl);
+
+  $entityControl.appendChild($plane1);
+  $entityControl.appendChild($plane2);
+  $entityControl.appendChild($plane3);
+
+  return $sceneEl;
 };
 
 renderThreeD();
 
-document.body.addEventListener('click',() => {
-
+document.body.addEventListener('click',function() {
   if (event.target.id === 'patagonia') {
     openWeatherQuery('patagonia');
   }
@@ -95,9 +91,16 @@ document.body.addEventListener('click',() => {
   else if (event.target.id === 'yosemite') {
     openWeatherQuery('yosemite');
   }
-} );
+  var cloud = document.getElementsByClassName('cloud');
+  var weather = document.getElementsByClassName('weather');
+  if(event.target.className == 'weather' || 'location'){
+    while(weather.length > 0){
+      weather[0].parentNode.removeChild(weather[0]);
+      }
+    }
+}, false);
 
-const openWeatherQuery = (city) => {
+function openWeatherQuery(city) {
  weather = fetch(`/weather/${event.target.id}`)
  weather
   .then((result) => result.json())
@@ -106,50 +109,73 @@ const openWeatherQuery = (city) => {
   .catch((error) => console.error(error))
 }
 
-var showWeather = (result) => {
-  var sceneEl = document.getElementById('scene');
+
+function showWeather(result) {
+  var $sceneEl = document.getElementById('scene');
 
   var obj = JSON.parse(result);
   //console.log(obj.weather[0].main);
-//  var $temp = document.createElement('a-entity');
-//  var $humid = document.createElement('a-entity');
-  var weather;
-  var cloud;
-  if(obj.weather[0].main == 'Cloud'){
-    weather = document.createElement('a-collada-model');
-    weather.setAttribute('position', '-1 2.5 0');
-    weather.setAttribute('src', '#cloud');
-    weather.setAttribute('id', 'cloud');
+  var $weather;
+  var $$cloud;
+  var tempNum = (obj.main.temp).toFixed(0);
+  var $temp = document.createElement('a-entity');
+  $temp.setAttribute('bmfont-text', `text: ${tempNum} Degrees; color: white`);
+  $temp.setAttribute('position', '-1 6.5 0');
+  $temp.setAttribute('scale', '3 3 3');
+  $temp.setAttribute('class', 'weather');
+
+  var humidNum = (obj.main.humidity).toFixed(0);
+  var $humidity = document.createElement('a-entity');
+  $humidity.setAttribute('bmfont-text', `text: humidity ${humidNum}%; color: white`);
+  $humidity.setAttribute('position', '0 -.2 0');
+  $humidity.setAttribute('scale', '1 1 1');
+  $humidity.setAttribute('class', 'weather');
+
+  var windNum = (obj.wind.speed).toFixed(0);
+  var $wind = document.createElement('a-entity');
+  $wind.setAttribute('bmfont-text', `text: wind ${windNum}mph; color: #9e9e9a`);
+  $wind.setAttribute('position', '0 -.2 0');
+  $wind.setAttribute('scale', '1 1 1');
+  $wind.setAttribute('class', 'weather');
+
+  if(obj.weather[0].main == "Clouds"){
+    $weather = document.createElement('a-collada-model');
+    $weather.setAttribute('position', '0 4 -2.5');
+    $weather.setAttribute('src', '#cloud');
+    $weather.setAttribute('class', 'weather');
+    $cloud = document.createElement('a-collada-model');
+    $cloud.setAttribute('position', '1.5 1 -1');
+    $cloud.setAttribute('src', '#cloud');
+    $cloud.setAttribute('class', 'weather');
+    $weather.appendChild($cloud);
   }
   else if(obj.weather[0].main == "Clear") {
-    cloud = document.createElement('a-collada-model');
-    cloud.setAttribute('position', '1.5 -1.5 1.5');
-    cloud.setAttribute('src', '#cloud');
-    cloud.setAttribute('class', 'sun');
-    weather = document.createElement('a-sphere');
-    weather.setAttribute('color', 'yellow');
-    weather.setAttribute('radius', '2');
-    weather.setAttribute('position', '0 4.5 -2.5');
-    weather.setAttribute('class', 'sun');
+    $weather = document.createElement('a-sphere');
+    $weather.setAttribute('color', 'yellow');
+    $weather.setAttribute('radius', '2');
+    $weather.setAttribute('position', '0 4.5 -2.5');
+    $weather.setAttribute('class', 'weather');
+    $cloud = document.createElement('a-collada-model');
+    $cloud.setAttribute('position', '1.5 -1.5 1.5');
+    $cloud.setAttribute('src', '#cloud');
+    $cloud.setAttribute('class', 'weather');
+    $weather.appendChild($cloud);
   }
-  weather.appendChild(cloud);
-  sceneEl.appendChild(weather);
+  var $animation = document.createElement('a-animation');
+  $animation.setAttribute('attribute', 'position');
+  $animation.setAttribute('direction', 'alternate');
+  $animation.setAttribute('to','0 3.7 -2.5');
+  $animation.setAttribute('dur', '2000');
+  $animation.setAttribute('repeat', 'indefinite');
 
-  return sceneEl;
+  //<a-animation attribute="position" to="0 2.2 -5" direction="alternate" dur="2000"
+  //repeat="indefinite"></a-animation>
+
+  $sceneEl.appendChild($weather);
+  $sceneEl.appendChild($temp);
+  $weather.appendChild($animation);
+  $temp.appendChild($humidity);
+  $humidity.appendChild($wind);
+  return $sceneEl;
 
 };
-
-document.body.addEventListener('click', () => {
-  var cloud = document.getElementsByClassName('cloud');
-  var sun = document.getElementsByClassName('sun');
-  if(event.target.className == 'sun'){
-    while(sun.length > 0){
-      sun[0].parentNode.removeChild(sun[0]);
-      }
-    }
-  if(event.target.className == 'cloud'){
-    while(cloud.length > 0){
-      cloud[0].parentNode.removeChild(cloud[0]);
-      }
-    }
-}, false);
