a0efcc864ae12bfb0a71321e620b7f9f28e6bf25
thienmdoan <thienmdoan@gmail.com>
2017-02-15 18:04:00 -0800

    User can make movements using degree radius.

--- client/index.js
+++ client/index.js
@@ -1,87 +1,6 @@
-function renderThreeD() {
-  var $sceneEl = document.getElementById('scene');
-  var $entityEl = document.createElement('a-plane');
-  $entityEl.setAttribute('id', 'platform')
-  $entityEl.setAttribute('color', 'tan');
-  $entityEl.setAttribute('height', '100');
-  $entityEl.setAttribute('width', '100');
-  $entityEl.setAttribute('rotation', '-90 0 0');
-
-  var $entityVid = document.createElement('a-video');
-  $entityVid.setAttribute('src', '#video');
-  $entityVid.setAttribute('width', '16');
-  $entityVid.setAttribute('height', '9');
-  $entityVid.setAttribute('position', '0 5 -10');
-  $entityVid.setAttribute('on', 'click');
-
-  var $entityControl = document.createElement('a-entity');
-  $entityControl.setAttribute('position', '0 0 0');
-  $entityControl.setAttribute('camera');
-  $entityControl.setAttribute('look-controls');
-  $entityControl.setAttribute('wasd-controls');
-
-  var $plane1 = document.createElement('a-image');
-  $plane1.setAttribute('src', 'patagonia.jpg');
-  $plane1.setAttribute('position', '0 2.5 -6');
-  $plane1.setAttribute('height','4');
-  $plane1.setAttribute('width', '5');
-  $plane1.setAttribute('id', 'patagonia');
-  $plane1.setAttribute('class', 'location');
-
-  var $plane2 = document.createElement('a-image');
-  $plane2.setAttribute('src', 'yosemite.jpg');
-  $plane2.setAttribute('position', '-5.5 2.5 -4');
-  $plane2.setAttribute('height','4');
-  $plane2.setAttribute('width', '5');
-  $plane2.setAttribute('rotation', '0 40 0');
-  $plane2.setAttribute('id', 'yosemite');
-  $plane2.setAttribute('class', 'location');
-
-  var $plane3 = document.createElement('a-image');
-  $plane3.setAttribute('src', 'joshuatree.jpg');
-  $plane3.setAttribute('position', '5.5 2.5 -4');
-  $plane3.setAttribute('height','4');
-  $plane3.setAttribute('width', '5');
-  $plane3.setAttribute('rotation', '0 -40 0');
-  $plane3.setAttribute('id', 'joshuatree');
-  $plane3.setAttribute('class', 'location');
-
-  var $sky = document.createElement('a-sky');
-  $sky.setAttribute('src', '#skyTexture');
-
-  var $animation = document.createElement('a-animation');
-  $animation.setAttribute('attribute', 'rotation');
-  $animation.setAttribute('delay', '2000');
-  $animation.setAttribute('to','360 0 0');
-
-  var $boss = document.createElement('a-entity');
-  $boss.setAttribute('text', `value: BoulderBoss; color: white`);
-  $boss.setAttribute('class', 'weather');
-  $boss.setAttribute('position', '5.5 6.5 -6.5');
-  $boss.setAttribute('align', 'center');
-  $boss.setAttribute('scale', '15 15 15');
-
-  var $welcome = document.createElement('a-entity');
-  $welcome.setAttribute('text', `value: Welcome!; color: white`);
-  $welcome.setAttribute('class', 'weather');
-  $welcome.setAttribute('position', '0 -0.05 0');
-  $welcome.setAttribute('scale', '.9 .9 .9');
-
-
-  $sceneEl.appendChild($entityEl);
-  $sceneEl.appendChild($sky);
-  //$sceneEl.appendChild($entityVid);
-  $sceneEl.appendChild($entityControl);
-  $sceneEl.appendChild($boss);
-  $boss.appendChild($welcome);
-  $welcome.appendChild($animation);
-  $entityControl.appendChild($plane1);
-  $entityControl.appendChild($plane2);
-  $entityControl.appendChild($plane3);
-
-  return $sceneEl;
-}
-
+const moving = require('./mover.js')
+const renderThreeD = require('./world.js')
+const showWeather = require('./weatherfunc.js')
 renderThreeD();
 
 document.body.addEventListener('mousedown', function() {
@@ -96,8 +15,34 @@ document.body.addEventListener('mousedown', function() {
     openWeatherQuery('yosemite');
   }
 
+  var cloud = document.getElementsByClassName('cloud');
+  var weather = document.getElementsByClassName('weather');
+  if(event.target.className == 'location'){
+    while(weather.length > 0){
+      weather[0].parentNode.removeChild(weather[0]);
+      }
+    }
+
+  if(event.target.className === 'weather' || 'location'){
+    // console.log('clicked!');
+   }
+  //else if(!event.target.className === 'weather' || 'location') {
+  const $camera = document.getElementById('camera');
+  const cam = $camera.getAttribute('rotation');
+  console.log('camera', cam.y);
+  moving(cam);
+  //}
+  event.stopPropagation();
 }, false);
 
+function openWeatherQuery(city) {
+ const weather = fetch(`/weather/${event.target.id}`);
+ weather
+  .then((result) => result.json())
+  .then((result) => showWeather(result))
+  .catch((error) => console.error(error));
+}
+/*
 document.body.addEventListener('mousedown', function() {
 
   var cloud = document.getElementsByClassName('cloud');
@@ -114,108 +59,18 @@ function openWeatherQuery(city) {
  weather
   .then((result) => result.json())
   .then((result) => showWeather(result))
-  //USE CONSOLE LOG TO CHECK THE DATA
-  //.then((result) => console.log(result))
   .catch((error) => console.error(error));
 }
 
-
-function showWeather(result) {
-  var $sceneEl = document.getElementById('scene');
-
-  var obj = JSON.parse(result);
-  //USE CONSOLE LOG TO CHECK IF THE DATA IS COMING THROUGH
-  //console.log(obj.weather[0].main);
-  var $weather;
-  var $cloud;
-  var tempNum = (obj.main.temp).toFixed(0);
-  var $temp = document.createElement('a-entity');
-  $temp.setAttribute('text', `value: ${tempNum} Degrees; color: white`);
-  $temp.setAttribute('position', '4 5.8 0');
-  $temp.setAttribute('align', 'center');
-  $temp.setAttribute('scale', '10 10 10');
-  $temp.setAttribute('class', 'weather');
-
-  var humidNum = (obj.main.humidity).toFixed(0);
-  var $humidity = document.createElement('a-entity');
-  $humidity.setAttribute('text', `value: humidity ${humidNum}%; color: white`);
-  $humidity.setAttribute('position', '0 -.05 0');
-  $humidity.setAttribute('align', 'center');
-  $humidity.setAttribute('scale', '1 1 1');
-  $humidity.setAttribute('class', 'weather');
-
-  var windNum = (obj.wind.speed).toFixed(0);
-  var $wind = document.createElement('a-entity');
-  $wind.setAttribute('text', `value: wind ${windNum}mph; color: #9e9e9a`);
-  $wind.setAttribute('position', '0 -.05 0');
-  $wind.setAttribute('align', 'center');
-  $wind.setAttribute('scale', '1 1 1');
-  $wind.setAttribute('class', 'weather');
-
-  if(obj.weather[0].main == "Clouds"){
-    $weather = document.createElement('a-collada-model');
-    $weather.setAttribute('position', '0 3 -2.5');
-    $weather.setAttribute('src', '#cloud');
-    $weather.setAttribute('class', 'weather');
-    $cloud = document.createElement('a-collada-model');
-    $cloud.setAttribute('position', '1.5 1 -1');
-    $cloud.setAttribute('src', '#cloud');
-    $cloud.setAttribute('class', 'weather');
-    $weather.appendChild($cloud);
+document.addEventListener('mousedown', function () {
+    if(event.target.className === 'weather' || 'location'){
+       console.log('clicked!');
+     }
+    else if(!event.target.className === 'weather' || 'location') {
+    const $camera = document.getElementById('camera');
+    const cam = $camera.getAttribute('rotation');
+    console.log('camera', cam.y);
+    moving(cam);
   }
-  else if(obj.weather[0].main == "Clear") {
-    $weather = document.createElement('a-sphere');
-    $weather.setAttribute('color', 'yellow');
-    $weather.setAttribute('radius', '2');
-    $weather.setAttribute('position', '0 3.5 -2.5');
-    $weather.setAttribute('class', 'weather');
-    $cloud = document.createElement('a-collada-model');
-    $cloud.setAttribute('position', '1.5 -1.5 1.5');
-    $cloud.setAttribute('src', '#cloud');
-    $cloud.setAttribute('class', 'weather');
-    $weather.appendChild($cloud);
-  }
-  else if(obj.weather[0].main == "Rain"){
-    $weather = document.createElement('a-entity');
-    $weather.setAttribute('position', '0 2.5 -2.5');
-    $weather.setAttribute('rotation', '0 0 0');
-    $weather.setAttribute('scale', '.2 .2 .2');
-    $weather.setAttribute('ply-model', `src: #rain`);
-    $weather.setAttribute('class', 'weather');
-
-    //<a-entity ply-model="src: #rain" position="-4 10 3" rotation="0 0 0" scale=".2 .2 .2"></a-entity>
-    $cloud = document.createElement('a-collada-model');
-    $cloud.setAttribute('position', '0 6.3 6.8');
-    $cloud.setAttribute('scale', '3.5 3.5 3.5');
-    $cloud.setAttribute('src', '#cloud');
-    $cloud.setAttribute('class', 'weather');
-    $weather.appendChild($cloud);
-  }
-
-  var $animation = document.createElement('a-animation');
-  $animation.setAttribute('attribute', 'position');
-  $animation.setAttribute('direction', 'alternate');
-  $animation.setAttribute('to','0 3.7 -2.5');
-  $animation.setAttribute('dur', '2000');
-  $animation.setAttribute('repeat', 'indefinite');
-
-  $sceneEl.appendChild($weather);
-  $sceneEl.appendChild($temp);
-  $weather.appendChild($animation);
-  $temp.appendChild($humidity);
-  $humidity.appendChild($wind);
-  return $sceneEl;
-
-}
-/*
-const sphere = document.getElementById('sphere');
-
-sphere.addEventListener('mousedown', function () {
-  console.log(event.target.getAttribute());
-/*  document.getElementById('camera');
-  if(event.target.id == "sphere") {
-    var position = JSON.stringify(sphere.position);
-    console.log(position);
-    //camera.setAttribute('position', sphere.position);
+});
 */
-//});
---
+++ client/world.js
@@ -0,0 +1,83 @@
+module.exports = function renderThreeD() {
+  var $sceneEl = document.getElementById('scene');
+  var $entityEl = document.createElement('a-plane');
+  $entityEl.setAttribute('id', 'platform')
+  $entityEl.setAttribute('color', 'tan');
+  $entityEl.setAttribute('height', '100');
+  $entityEl.setAttribute('width', '100');
+  $entityEl.setAttribute('rotation', '-90 0 0');
+
+  var $entityVid = document.createElement('a-video');
+  $entityVid.setAttribute('src', '#video');
+  $entityVid.setAttribute('width', '16');
+  $entityVid.setAttribute('height', '9');
+  $entityVid.setAttribute('position', '0 5 -10');
+  $entityVid.setAttribute('on', 'click');
+
+  var $entityControl = document.createElement('a-entity');
+  $entityControl.setAttribute('position', '0 0 0');
+  $entityControl.setAttribute('camera');
+  $entityControl.setAttribute('look-controls');
+  $entityControl.setAttribute('wasd-controls');
+
+  var $plane1 = document.createElement('a-image');
+  $plane1.setAttribute('src', '#patagonia');
+  $plane1.setAttribute('position', '0 5.5 -6');
+  $plane1.setAttribute('height','4');
+  $plane1.setAttribute('width', '5');
+  $plane1.setAttribute('id', 'patagonia');
+  $plane1.setAttribute('class', 'location');
+
+  var $plane2 = document.createElement('a-image');
+  $plane2.setAttribute('src', '#yosemite');
+  $plane2.setAttribute('position', '-5.5 5.5 -4');
+  $plane2.setAttribute('height','4');
+  $plane2.setAttribute('width', '5');
+  $plane2.setAttribute('rotation', '0 40 0');
+  $plane2.setAttribute('id', 'yosemite');
+  $plane2.setAttribute('class', 'location');
+
+  var $plane3 = document.createElement('a-image');
+  $plane3.setAttribute('src', '#joshuatree');
+  $plane3.setAttribute('position', '5.5 5.5 -4');
+  $plane3.setAttribute('height','4');
+  $plane3.setAttribute('width', '5');
+  $plane3.setAttribute('rotation', '0 -40 0');
+  $plane3.setAttribute('id', 'joshuatree');
+  $plane3.setAttribute('class', 'location');
+
+  var $sky = document.createElement('a-sky');
+  $sky.setAttribute('src', '#skyTexture');
+
+  var $animation = document.createElement('a-animation');
+  $animation.setAttribute('attribute', 'rotation');
+  $animation.setAttribute('delay', '2000');
+  $animation.setAttribute('to','360 0 0');
+
+  var $boss = document.createElement('a-entity');
+  $boss.setAttribute('text', `value: BoulderBoss; color: white`);
+  $boss.setAttribute('class', 'weather');
+  $boss.setAttribute('position', '5.5 9.5 -6.5');
+  $boss.setAttribute('align', 'center');
+  $boss.setAttribute('scale', '15 15 15');
+
+  var $welcome = document.createElement('a-entity');
+  $welcome.setAttribute('text', `value: Welcome!; color: white`);
+  $welcome.setAttribute('class', 'weather');
+  $welcome.setAttribute('position', '0 -0.05 0');
+  $welcome.setAttribute('scale', '.9 .9 .9');
+
+
+  $sceneEl.appendChild($entityEl);
+  $sceneEl.appendChild($sky);
+  //$sceneEl.appendChild($entityVid);
+  $sceneEl.appendChild($entityControl);
+  $sceneEl.appendChild($boss);
+  $boss.appendChild($welcome);
+  $welcome.appendChild($animation);
+  $entityControl.appendChild($plane1);
+  $entityControl.appendChild($plane2);
+  $entityControl.appendChild($plane3);
+
+  return $sceneEl;
+};
