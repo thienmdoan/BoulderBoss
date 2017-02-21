52a9dc3abf33946b296650a89a6bb4d14e236e4d
Thien <Thien@ThienMac.local>
2017-02-21 13:39:17 -0800

    Testing update.

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,11 @@
 const {expect} = require('chai');
 const app = require('../index.js');
 const request = require('request');
+const world = require('../client/components/world.js');
+const mover = require('../client/components/mover.js');
+const target = require('../client/components/target.js');
+const character = require('../client/components/character.js');
+const weather = require('../client/components/weatherfunc.js');
 
 let server;
 const TEST_PORT = '3000';
@@ -30,4 +35,34 @@ describe('API', () => {
       server.close();
     });
 
+});
+
+describe ('world', () => {
+  it('should return back an object with properties for the scene', () => {
+    {};
+  });
+});
+
+describe ('mover', () => {
+  it('should return back an object with properties for the movement', () => {
+    {};
+  });
+});
+
+describe ('target', () => {
+  it('should return back an object with properties for the climb', () => {
+    {};
+  });
+});
+
+describe ('character', () => {
+  it('should return back an object with properties for the characters', () => {
+    {};
+  });
+});
+
+describe ('weather', () => {
+  it('should parse the response from the weather given by the server', () => {
+    expect(true).to.be.true;
+  });
 });
